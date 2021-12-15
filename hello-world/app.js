b646b00315a365bfc7ea18cac8edefa34c70ac25
cclark64 <curliss.clark@post.tele.dk>
2021-12-15 18:13:12 +0100

    Adding Jenkins pipeline config.

--- hello-world/app.js
+++ hello-world/app.js
@@ -21,7 +21,7 @@ exports.lambdaHandler = async (event, context) => {
     response = {
       statusCode: 200,
       body: JSON.stringify({
-        message: "Curliss",
+        message: "Curliss again",
         // location: ret.data.trim()
       }),
     };
