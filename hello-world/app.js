b53e1892b20f369def33e4af65070f9e40d3b019
cclark64 <cclark64@users.noreply.github.com>
2021-12-21 08:37:13 +0100

    Update app.js

--- hello-world/app.js
+++ hello-world/app.js
@@ -21,7 +21,7 @@ exports.lambdaHandler = async (event, context) => {
     response = {
       statusCode: 200,
       body: JSON.stringify({
-        message: "Hello World 4",
+        message: "Hello World 5",
         // location: ret.data.trim()
       }),
     };
